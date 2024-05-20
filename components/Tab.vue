50f3e8a1eb77bf92b6ea3dd5d73caa8d65436d67
juniaraldi <aldinugraha708@gmail.com>
2024-05-21 01:37:27 +0700

    first commit

---
+++ components/Tab.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="{ 'bg-blue-300': thi }" class="max-w-screen-xl p-2 mx-auto lg:py-16">
+    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+      <li class="mr-2">
+        <NuxtLink :to="{ name: 'index' }" class="inline-block px-4 py-3 text-sm border border-gray-200 rounded-lg hover:text-white hover:bg-blue-600">
+          Automated Quiz
+        </NuxtLink>
+      </li>
+      <!-- <li class="mr-2">
+        <NuxtLink :to="{ name: 'multi' }" class="inline-block px-4 py-3 text-sm border border-gray-200 rounded-lg hover:text-white hover:bg-blue-600">
+          Multi AES
+        </NuxtLink>
+      </li> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      thi: false, // Set this to true to activate the blue background
+    };
+  },
+};
+</script>
+
+<style scoped>
+.router-link-active {
+  @apply bg-blue-600 text-white font-bold;
+}
+.router-link-active:hover {
+  @apply bg-blue-600 font-medium;
+}
+</style>
+// or
+<style>
+.my-link.router-link-active {
+  @apply bg-blue-600 font-bold;
+}
+.my-link.router-link-active:hover {
+  @apply bg-blue-600 font-medium;
+}
+</style>
---
+++ layouts/nav.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <nav class="p-4 bg-white border-b-2 border-gray-200">
+    <div
+      class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto"
+    >
+      <NuxtLink :to="{ name: 'index' }" class="flex items-center">
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          class="h-8 mr-3"
+          alt="Flowbite Logo"
+        />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap "
+          >Automated Generate Quiz</span
+        >
+      </NuxtLink>
+    </div>
+  </nav>
+  <slot/>
+
+<!--Footer container-->
+<footer class="text-center text-white bg-white border-grey dark:bg-neutral-600">
+  
+
+  <!--Copyright section-->
+  <div
+    class="p-4 text-center border-t-2 bg-gray-50 text-neutral-700 border-grey">
+    © 2024 Copyright:
+    <a
+      class="text-neutral-800 "
+      href="https://github.com/juniaraldi"
+      >jun</a
+    >
+  </div>
+</footer>
+</div>
+</template>
